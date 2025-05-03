cff9a469a332c91a901f72f1dc502f41a0b8aef2
wel055 <130526509+wel055@users.noreply.github.com>
2025-05-02 17:50:33 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -10,13 +10,10 @@ const title = document.querySelector('.projects-title');
 if (title) title.textContent = `${projects.length} Projects`;
 
 /* ---- arc with D3 ---- */
-const arc = d3.arc().innerRadius(0).outerRadius(50)({
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
     startAngle: 0,
-    endAngle: 2 * Math.PI,   // full circle
+    endAngle: 2 * Math.PI,
   });
   
-  /* append it to our existing SVG */
-  d3.select('#projects-plot')
-    .append('path')
-    .attr('d', arc)
-    .attr('fill', 'red');
+/* append it to our existing SVG */
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
