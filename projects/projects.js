3b20f3a0293669c4e5e354ef0cd66aa3318527be
wel055 <130526509+wel055@users.noreply.github.com>
2025-05-02 17:49:00 -0700

    updated circle

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects          = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
@@ -7,3 +8,15 @@ renderProjects(projects, projectsContainer, 'h2');
 
 const title = document.querySelector('.projects-title');
 if (title) title.textContent = `${projects.length} Projects`;
+
+/* ---- arc with D3 ---- */
+const arc = d3.arc().innerRadius(0).outerRadius(50)({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,   // full circle
+  });
+  
+  /* append it to our existing SVG */
+  d3.select('#projects-plot')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', 'red');
